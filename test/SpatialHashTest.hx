c6d1dcb1826297e5ff6def4edeb4fe98b4bd8438
Grant Mathews <johnfn@gmail.com>
2013-01-19 19:11:49 -0800

    Add another test.

--- test/SpatialHashTest.hx
+++ test/SpatialHashTest.hx
@@ -19,6 +19,8 @@ class SpatialHashTest extends haxe.unit.TestCase {
         .loadSpritesheet(AllTests.testAnimation, d, w);
     var e2: Entity = new Entity(25, 0, 25, 25)
         .loadSpritesheet(AllTests.testAnimation, d, w);
+    var e3: Entity = new Entity(12, 0, 25, 25)
+        .loadSpritesheet(AllTests.testAnimation, d, w);
 
     sh.add(e1);
     sh.add(e2);
@@ -26,8 +28,6 @@ class SpatialHashTest extends haxe.unit.TestCase {
     assertEquals(sh.getColliders(e1).length, 0);
     assertEquals(sh.getColliders(e2).length, 0);
 
-    var e3: Entity = new Entity(12, 0, 25, 25)
-        .loadSpritesheet(AllTests.testAnimation, d, w);
 
     sh.add(e3);
 
@@ -36,6 +36,46 @@ class SpatialHashTest extends haxe.unit.TestCase {
 
     //assertEquals(sh.getColliders(e1).first(), e3);
     //assertEquals(sh.getColliders(e2).first(), e3);
+  }
+
+  public function testBig() {
+    var sh:SpatialHash = new SpatialHash([]);
+
+    var e1: Entity = new Entity(0, 0, 25, 25)
+        .loadSpritesheet(AllTests.testAnimation, new Vec(50, 50), w);
+
+    var e2: Entity = new Entity(0, 0, 25, 25)
+        .loadSpritesheet(AllTests.testAnimation, d, w);
+    var e3: Entity = new Entity(25, 0, 25, 25)
+        .loadSpritesheet(AllTests.testAnimation, d, w);
+    var e4: Entity = new Entity(0, 25, 25, 25)
+        .loadSpritesheet(AllTests.testAnimation, d, w);
+    var e5: Entity = new Entity(25, 25, 25, 25)
+        .loadSpritesheet(AllTests.testAnimation, d, w);
+
+    sh.add(e1);
+    sh.add(e2);
+    sh.add(e3);
+    sh.add(e4);
+    sh.add(e5);
+
+    assertEquals(sh.getColliders(e1).length, 4);
+
+    assertTrue(sh.getColliders(e1).exists(e2));
+    assertTrue(sh.getColliders(e1).exists(e3));
+    assertTrue(sh.getColliders(e1).exists(e4));
+
+    assertEquals(sh.getColliders(e2).length, 1);
+    assertEquals(sh.getColliders(e2).first(), e1);
+
+    assertEquals(sh.getColliders(e3).length, 1);
+    assertEquals(sh.getColliders(e3).first(), e1);
+
+    assertEquals(sh.getColliders(e4).length, 1);
+    assertEquals(sh.getColliders(e4).first(), e1);
+
+    assertEquals(sh.getColliders(e5).length, 1);
+    assertEquals(sh.getColliders(e5).first(), e1);
   }
 
   public override function globalTeardown() {
